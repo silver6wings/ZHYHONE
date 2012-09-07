4e5821a847e01c3c0a9cb491265676eea61f6aec
silver6wings <silver6wings@silver6wingsmatoMacBook.local>
2012-09-07 23:48:29 +0800

    Initial Commit

---
+++ ZHYHONE/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZHYHONE
+//
+//  Created by silver6wings silver6wings on 12-9-7.
+//  Copyright (c) 2012年 BUAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZHYHONE/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZHYHONE
+//
+//  Created by silver6wings silver6wings on 12-9-7.
+//  Copyright (c) 2012年 BUAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
